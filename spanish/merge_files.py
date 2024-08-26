4a4f981fc11420c9ac48181f240fa50a74904952
Alfonso <amayuelas@ucsb.edu>
2024-08-26 05:46:37 -0700

    changes answers to numbers

--- spanish/merge_files.py
+++ spanish/merge_files.py
@@ -2,8 +2,8 @@
 import os
 import json
 
-input_dir = "data/spanish/mcq-answers"
-output_path = "data/spanish/mcq-answers-all.jsonl"
+input_dir = "../data/spanish/mcq-answers"
+output_path = "../data/spanish/mcq-answers-all.jsonl"
 
 files = [file for file in os.listdir(input_dir) if file.endswith('.jsonl')]
 
@@ -15,6 +15,24 @@ for file in files:
         data = [json.loads(line) for line in data]
         all_questions.extend(data)
 
+# convert answers from letter to integer
+out_questions = []
+for q in all_questions:
+    if q['answer'] == None:
+        continue
+
+    if q['answer'].lower() == 'a':
+        q['answer'] = 1
+    elif q['answer'].lower() == 'b':
+        q['answer'] = 2
+    elif q['answer'].lower() == 'c':
+        q['answer'] = 3
+    elif q['answer'].lower() == 'd':
+        q['answer'] = 4
+
+    out_questions.append(q)
+
+
 with open(output_path, 'w') as f:
-    for d in all_questions:
+    for d in out_questions:
         f.write(json.dumps(d, ensure_ascii=False) + '\n')
